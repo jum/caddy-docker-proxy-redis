e66f92ef6c7dabcd49796b16c4a879580e450701
Jens-Uwe Mager <jum@anubis.han.de>
2023-12-01 18:12:44 +0100

    Revert back to tagging the docker image with the branch name.

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 DOCKER_PREFIX=jumager/caddy
-#branch=$(basename $(git rev-parse --abbrev-ref HEAD))
-branch=develop
+branch=$(basename $(git rev-parse --abbrev-ref HEAD))
 docker buildx build --platform linux/arm64,linux/amd64 -t ${DOCKER_PREFIX}:${branch} -f Dockerfile --push .
