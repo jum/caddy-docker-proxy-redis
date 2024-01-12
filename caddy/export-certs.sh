a5176e9ac429e093d2456ba68868c37d7ae9a1f3
Jens-Uwe Mager <jum@anubis.han.de>
2024-01-12 12:38:35 -0500

    Do not use the interactive stuff just for exporting, it might interfere with running that command in other shell scripts.

--- caddy/export-certs.sh
+++ caddy/export-certs.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-if test -t 0
-then
-	ARGS=-t
-fi
-docker exec -i $ARGS caddy sh -c "caddy --config /config/Caddyfile storage export -o /data/certs.tar"
+docker exec $ARGS caddy sh -c "caddy --config /config/Caddyfile storage export -o /data/certs.tar"
