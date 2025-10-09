a0009ba185dcfb5302b99a44356efa04c72dcea5
Jens-Uwe Mager <jum@anubis.han.de>
2025-10-09 13:07:37 +0200

    Keep the last old sql dump.

--- postgres/dumpdb.sh
+++ postgres/dumpdb.sh
@@ -1,1 +1,7 @@
-docker exec postgres sh -c 'exec pg_dumpall -U postgres -c ' >dbdump.psqldump
+#!/bin/sh
+SQLDUMP=dbdump.psqldump
+if test -f $SQLDUMP
+then
+	mv $SQLDUMP ${SQLDUMP}.old
+fi
+docker exec postgres sh -c 'exec pg_dumpall -U postgres -c ' >$SQLDUMP
