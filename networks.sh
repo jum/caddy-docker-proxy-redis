1adbe709b87039727873587efa86017e06407ba4
Jens-Uwe Mager <jum@anubis.han.de>
2026-06-02 14:42:47 -0400

    Some installation need ipv6nat for true ipv6 in docker.

--- networks.sh
+++ networks.sh
@@ -4,4 +4,17 @@ docker network create caddy
 
 # If containers should have ipv6 available and the host is
 # properly configured, use this instead:
+
 #docker network create --ipv6 --subnet 2001:db8::/64 caddy
+
+# On some systems that are very strict in their network
+# routing (e.g. oracle oci instances) you may need use
+# this instead and run the ipv6nat service.
+
+#docker network create \
+#	--ipv6 \
+#	--subnet=172.20.0.0/16 \
+#	--subnet=fd00:cadd:eeee::/64 \
+#	-o "com.docker.network.bridge.enable_icc=true" \
+#	-o "com.docker.network.options.experimental.iptables=true" \
+#	caddy
